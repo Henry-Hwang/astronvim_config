1db6de1f53bf97d893825b86b9ccddebd77976b5
Henry Huang <Henry.hwang@outlook.com>
2023-04-20 17:03:26 +0800

    Update mason.lua

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "clangd", "python-lsp-server" },
+      ensure_installed = { "clangd", "lua-ls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
