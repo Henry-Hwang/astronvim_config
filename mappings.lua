9ddf2e2c8a7096071d371f3ecc8fd5d6d0095a3c
Henry Huang <Henry.hwang@outlook.com>
2023-04-20 10:24:59 +0800

    Update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -16,12 +16,25 @@ return {
       desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
+    -- thiswith:  is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    ["<leader>r"] = { ":%s/<C-r><C-w>/<C-r><C-w>/gc" },
+    ["<leader>ef"] = { "<cmd>Neotree float<cr>", desc = "Neotree float" },
+    ["<leader>r"] = { ":%s/<C-r><C-w>/<C-r><C-w>/gc", desc = "Replace word" },
+    ["ff"] = { "/<C-r><C-w>", desc = "Search word"},
+    ["<C-a>"] = { ":Telescope buffers<cr>", desc = "Show buffers" },  -- change description but the same command
+    ["<C-s>"] = { ":Telescope find_files<cr>", desc = "Find files" },  -- change description but the same command
+    ["<C-e>"] = { ":e %:h", desc = "Path to file" },  -- change description but the same command
+    ["\\"] = { "<cmd>split<cr>", desc = "Horizontal split" },
+    ["|"] = { "<cmd>vsplit<cr>", desc = "Vertical split" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-  },
+    ["<leader>dr"] = {
+      function()
+        local word = vim.fn.expand "<cword>"
+        local rp = vim.fn.input "Replace with: "
+        vim.cmd("%s/" .. word .. "/" .. rp .. "/g")
+      end,
+    },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
