09a9825d751a9d0aedefc76d7226134fa70566dc
Henry Huang <Henry.hwang@outlook.com>
2023-04-20 10:28:47 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ return {
 
   -- Set colorscheme to use
   colorscheme = "astrodark",
-
+  startinsert = false,
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
